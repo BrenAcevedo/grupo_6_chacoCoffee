85b5b2e4349b06fb817eaa320c56e29ef3c5136e
Edgardo Jovanovich <edgardoj.se@gmail.com>
2022-01-06 11:59:43 -0300

    Inicie el proyecto, cree carpeta views y public

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
+
+app.set('port',process.env.PORT || 3000)
+
+app.listen(app.get('port'), () => {
+    console.log('Servidor corriendo en puerto '+app.get('port'))
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
