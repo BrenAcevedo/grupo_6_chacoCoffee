7d0b82be02f4630458b2e6d567a30b2b4a8887e2
Edgardo Jovanovich <edgardoj.se@gmail.com>
2022-02-18 20:38:24 -0300

    Cambios en vistas: create, edit, detail y en  models products

--- src/models/product.js
+++ src/models/product.js
@@ -1,28 +1,27 @@
-const {readFileSync, writeFileSync, unlinkSync, existsSync} = require('fs');
+const {readFileSync, writeFileSync} = require('fs');
 const {resolve, format} = require('path');
 
 const model = {
-    file: resolve(__dirname,'../data/','products.json'), // Con que archivo se comunica.
-    read: () => readFileSync(model.file), // Leer el contenido del archivo.
-    list: () => JSON.parse(model.read()), // Convertimos a algo que podamos entender, manipular desde JS.
-    convert: data => JSON.stringify(data,null,2), // Convierto cualquier info a JSON null y 2 mejoran la visualización
-    write: data => writeFileSync(model.file, model.convert(data)), // Utiliza convert para guardar o sobreescribir el archivo
-    all: () => model.list().filter(p => p.stock > 0), // p recorreria cada uno del objeto que tengo en .json
+    file: resolve(__dirname,'../data/','products.json'), 
+    read: () => readFileSync(model.file), 
+    list: () => JSON.parse(model.read()), 
+    convert: data => JSON.stringify(data,null,2),
+    write: data => writeFileSync(model.file, model.convert(data)),
+    all: () => model.list().filter(p => p.stock > 0), 
     match: (propiedad,valor) => model.all().find(producto => producto[propiedad] == valor),
-    generate: data => Object({  //Armamos como el esqueleto. 
-        id: model.list().length > 0 ? model.list().sort((a,b) => a.id < b.id ? -1 : a.id > b.id ? 1 : 0).pop().id + 1 : 1,  // Acá calculamos el id, si existe alguno tomamos el numero del ultimo y le sumamos 1 si no es 1 . No uso all porque eso me filtra por stock y no todos los que tengo en realidad.
+    generate: data => Object({  
+        id: model.list().length > 0 ? model.list().sort((a,b) => a.id < b.id ? -1 : a.id > b.id ? 1 : 0).pop().id + 1 : 1,  
         nombre: data.nombre,
-        precio: Number(data.precio), // Convertimos el valor en número.
+        precio: Number(data.precio),
         descripcion: data.descripcion,
         variedad: data.variedad,
         olfato: data.olfato,
-        peso: data.peso,
+        peso: data.peso && Array.isArray(data.peso) ? data.peso : [data.peso],
         stock: Number(data.stock),
         categoria: data.categoria,
         informacion: data.informacion,
         packaging: data.packaging,
         imagen: data.files && data.files.length > 0 ? data.files.map(file => file.filename) : null
-        // sizes: data.sizes && data.sizes.length > 0 ? data.sizes : [] - Si quiero distintos talles por ejemplo.
     }),
     create: data => {
         let lista = model.list().sort((a,b) => a.id < b.id ? -1 : a.id > b.id ? 1 : 0)
@@ -38,12 +37,12 @@ const model = {
                 producto.descripcion = data.descripcion,
                 producto.variedad = data.variedad,
                 producto.olfato = data.olfato,
-                producto.peso = data.peso,
+                producto.peso = data.peso && Array.isArray(data.peso) ? data.peso : [data.peso],
                 producto.stock = Number(data.stock),
                 producto.categoria = data.categoria,
                 producto.informacion = data.informacion
                 producto.packaging = data.packaging
-                producto.imagen = data.files && data.files.length > 0 ? data.files.map(file => file.filename) : null
+                producto.imagen = data.files && data.files.length > 0 ? data.files.map(file => file.filename) : producto.imagen
                 return producto;
             }
             return producto;
